532003adf1033f09175fec403de9ea3bb73fc60f
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-06-18 11:51:14 -0400

    Write 3 solutions to the fibonacci sequence First solution uses array storage Second solution uses iteration without storage Third solution uses recursive function calls Test all 3 solutions in node

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,33 @@
+//initial iteration solve using an array
 function fib(n){
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    let array = [1, 1];
+    for(let i = 0; i < n; i++){
+        const newNum = array[array.length - 1] + array[array.length - 2];
+        array.push(newNum);
+    }
+    return array[array.length - 3];
 }
+console.log(fib(8));
 
-console.log(fib(8))
+//iteration solve that's better than array version because array version stores every single value
+function fibonacci(n) {
+    if (n <= 2) return 1;
+
+    let a = 1;
+    let b = 1;
+
+    for(let i = 3; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
+}
+console.log(fibonacci(8));
+
+//recursive version and inefficent for speed
+function fibsRec(n){
+    return n <= 1 ? n : fibsRec(n - 1) + fibsRec(n - 2);
+}
+console.log(fibsRec(8));
